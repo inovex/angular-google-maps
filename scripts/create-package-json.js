d79b74334ec4b4e0b57a17f29dab95178cc40207
Jens Fahnenbruck <jens.fahnenbruck@inovex.de>
2017-06-20 12:10:12 +0200

    try to split

--- scripts/create-package-json.js
+++ scripts/create-package-json.js
@@ -4,7 +4,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const pkgNames = ['core'];
+const pkgNames = ['core', 'clusterer'];
 
 pkgNames.forEach(function(pkgName) {
   const basePkgJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
@@ -12,12 +12,20 @@ pkgNames.forEach(function(pkgName) {
   // define the package name
   basePkgJson.name = `@agm/${pkgName}`
 
+  const pkgJsonFile = `./src/${pkgName}/package.json`;
+  if (fs.existsSync(pkgJsonFile)) {
+    const pkgJson = JSON.parse(fs.readFileSync(pkgJsonFile, 'utf8'));
+    Object.keys(pkgJson).forEach(key => {
+      basePkgJson[key] = pkgJson[key];
+    });
+  }
+
   // remove scripts
   delete basePkgJson.scripts;
 
   // remove devDependencies (as there are important for the sourcecode only)
   delete basePkgJson.devDependencies;
-  
+
   basePkgJson.dependencies = {};
 
   const filepath = path.join(__dirname, `../dist/${pkgName}/package.json`);
